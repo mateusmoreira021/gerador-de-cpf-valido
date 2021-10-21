61718a7b8ad9b997898af104cedbf681d0e0a9e9
Mateus Moreira <mateusmoreira024@gmail.com>
2021-10-21 12:11:24 -0300

    gerador de cpf

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import GeraCPF from './modules/GeraCPF'
+
+import './assets/css/style.css';
+
+
+(function(){
+    const gera = new GeraCPF();
+    const resultado = document.querySelector('.resultado');
+
+    resultado.innerHTML = gera.geraNovoCpf();
+})();
